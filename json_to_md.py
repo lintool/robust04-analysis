d59e55e0593d6a7397b35db453810b346d1a828d
lintool <jimmylin@uwaterloo.ca>
2019-05-16 09:47:46 -0400

    Added empty entries, fixed broken link.

--- json_to_md.py
+++ json_to_md.py
@@ -26,18 +26,27 @@ def main():
         f.write(header)
         f.write('\n\n')
 
-        f.write('| paper | is neural paper | baseline AP | best AP |  \n')
-        f.write(':-------|-----------------|-------------|----------  \n')
+        f.write('| paper | is neural paper | baseline AP | best AP \n')
+        f.write(':-------|-----------------|:------------|:--------\n')
 
         for single_paper_info in paper_info:
+            baseline = ''
+            best = ''
+
+            # Even if an entry doesn't have AP scores, we want to still show, even as empty entry
+            if 'AP' in single_paper_info['baseline']:
+                baseline = single_paper_info['baseline']['AP']
+
+            if 'AP' in single_paper_info['best']:
+                best = single_paper_info['best']['AP']
+
             try:
                 f.write(
-                    '| [{}]({}) | {} | {} | {} |  \n'.format(
+                    '| [{}]({}) | {} | {} | {}\n'.format(
                                                     single_paper_info['short_cite'],
                                                     single_paper_info['cached_pdf'],
                                                     single_paper_info['is_nn_paper'],
-                                                    single_paper_info['baseline']['AP'],
-                                                    single_paper_info['best']['AP']
+                                                    baseline, best
                                                 )
                 )
 
